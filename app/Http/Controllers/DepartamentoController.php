ed1a5e8d7788a57351bb6173d87d1f523b0c8988
SAMUEL G <samuelgarza1029@gmail.com>
2025-02-20 14:13:42 -0600

    Se importa el Facade DB

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -6,6 +6,7 @@ use App\Models\Departamento;
 use App\Models\DepartamentoView;
 use App\Models\ObjResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 
